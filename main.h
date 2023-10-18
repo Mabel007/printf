aa7388e698170a3699c1fb56e52c346c4d2fe391
lambertoo <rulindanal@gmail.com>
2023-10-18 04:42:29 -0700

    pushing main .h

--- main.h
+++ main.h
@@ -31,3 +31,24 @@ typedef struct Format{
 int _printf(const char *format, ...);
 int handlePrint(const char *
 fmt, int *ind,val_list list, char buffer[], int flags,int width, int precision, int size);
+
+/* function prototype for various print functions */
+int printChar(va_list args. char buffer[], int flags, int width, int precision, int size);
+int printString(va_list args, char buffer[], int flags, int width, int precision, int size);
+int printPercent(va_list args, char buffer[], int flags, int width,int precision, int size);
+
+/*adding prototypes for other functions*/
+
+int getFlags(const char *format, int *index);
+int getWidth(const char *format, int *index, va_list args);
+int getPrecision(const char *format, int *index, va_list args);
+int getSize(const char *format, int *index);
+
+/*Ultility functions */
+int isPrintable(char c);
+int appendHexCode(char asciiCode, char buffer[], int index);
+int isDigit(char c);
+long int convertSizeNumber(long int num, it size);
+long int convewrtSizeUnsigned(unsigned long int num, int size);
+
+#endif
