a12a7322aeb568e2d26b39ae99629db0d24b62a1
lambertoo <rulindanal@gmail.com>
2023-10-19 04:20:17 -0700

    creating custome Printf function

--- _printf.c
+++ _printf.c
@@ -1,37 +1,32 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
 
-int _printf(const char *format, ...) {
-    va_list args;
-    int count = 0;
-    va_start(args, format);
+/**
+ * _printf - implementation of the inbuilt printf
+ * @format: the format specifier
+ * Return: the formated string
+ */
 
-    while (*format) {
-        if (*format == '%') {
-            format++;
-            if (*format == 'c') {
-                int c = va_arg(args, int);
-                putchar(c);
-                count++;
-            } else if (*format == 's') {
-                char *str = va_arg(args, char *);
-                while (*str) {
-                    putchar(*str);
-                    str++;
-                    count++;
-                }
-            } else if (*format == '%') {
-                putchar('%');
-                count++;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+int _printf(const char *format, ...)
+{
+	int printed = 0;
+	va_list args;
 
-    va_end(args);
-    return count;
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			printed = selector(format, args, printed);
+			format++;
+		}
+		else
+		{
+			_putchar(*format);
+			printed++;
+			format++;
+		}
+	}
+	va_end(args);
+	return (printed);
 }
